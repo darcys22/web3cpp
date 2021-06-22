8aee8a7b51897c7ef35d73ed5393f82da383dba8
Sean Darcy <darcys22@gmail.com>
2021-06-23 09:27:44 +1000

    initial commit libraries added

---
+++ build/external/CMakeFiles/cpr.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sean/web3cpp/external/cpr/cpr/auth.cpp" "/home/sean/web3cpp/build/external/CMakeFiles/cpr.dir/cpr/cpr/auth.cpp.o"
+  "/home/sean/web3cpp/external/cpr/cpr/bearer.cpp" "/home/sean/web3cpp/build/external/CMakeFiles/cpr.dir/cpr/cpr/bearer.cpp.o"
+  "/home/sean/web3cpp/external/cpr/cpr/cookies.cpp" "/home/sean/web3cpp/build/external/CMakeFiles/cpr.dir/cpr/cpr/cookies.cpp.o"
+  "/home/sean/web3cpp/external/cpr/cpr/cprtypes.cpp" "/home/sean/web3cpp/build/external/CMakeFiles/cpr.dir/cpr/cpr/cprtypes.cpp.o"
+  "/home/sean/web3cpp/external/cpr/cpr/curl_container.cpp" "/home/sean/web3cpp/build/external/CMakeFiles/cpr.dir/cpr/cpr/curl_container.cpp.o"
+  "/home/sean/web3cpp/external/cpr/cpr/curlholder.cpp" "/home/sean/web3cpp/build/external/CMakeFiles/cpr.dir/cpr/cpr/curlholder.cpp.o"
+  "/home/sean/web3cpp/external/cpr/cpr/error.cpp" "/home/sean/web3cpp/build/external/CMakeFiles/cpr.dir/cpr/cpr/error.cpp.o"
+  "/home/sean/web3cpp/external/cpr/cpr/multipart.cpp" "/home/sean/web3cpp/build/external/CMakeFiles/cpr.dir/cpr/cpr/multipart.cpp.o"
+  "/home/sean/web3cpp/external/cpr/cpr/parameters.cpp" "/home/sean/web3cpp/build/external/CMakeFiles/cpr.dir/cpr/cpr/parameters.cpp.o"
+  "/home/sean/web3cpp/external/cpr/cpr/payload.cpp" "/home/sean/web3cpp/build/external/CMakeFiles/cpr.dir/cpr/cpr/payload.cpp.o"
+  "/home/sean/web3cpp/external/cpr/cpr/proxies.cpp" "/home/sean/web3cpp/build/external/CMakeFiles/cpr.dir/cpr/cpr/proxies.cpp.o"
+  "/home/sean/web3cpp/external/cpr/cpr/proxyauth.cpp" "/home/sean/web3cpp/build/external/CMakeFiles/cpr.dir/cpr/cpr/proxyauth.cpp.o"
+  "/home/sean/web3cpp/external/cpr/cpr/response.cpp" "/home/sean/web3cpp/build/external/CMakeFiles/cpr.dir/cpr/cpr/response.cpp.o"
+  "/home/sean/web3cpp/external/cpr/cpr/session.cpp" "/home/sean/web3cpp/build/external/CMakeFiles/cpr.dir/cpr/cpr/session.cpp.o"
+  "/home/sean/web3cpp/external/cpr/cpr/timeout.cpp" "/home/sean/web3cpp/build/external/CMakeFiles/cpr.dir/cpr/cpr/timeout.cpp.o"
+  "/home/sean/web3cpp/external/cpr/cpr/unix_socket.cpp" "/home/sean/web3cpp/build/external/CMakeFiles/cpr.dir/cpr/cpr/unix_socket.cpp.o"
+  "/home/sean/web3cpp/external/cpr/cpr/util.cpp" "/home/sean/web3cpp/build/external/CMakeFiles/cpr.dir/cpr/cpr/util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CPR_CURL_NOSIGNAL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external/cpr/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
